e5a620c79889c1810fce8a90bc387c0e038c7cb1
BarathRaj26 <151170282+BarathRaj26@users.noreply.github.com>
2024-01-22 20:33:07 +0530

    Time: 2 ms (97.74%) | Memory: 45.1 MB (69.63%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int[] freq=new int[nums.length+1];
-        for(int n: nums){ //counting freq of every number
+        for(int n: nums){ 
             freq[n]++;
         }
         int dup=0,miss=0;
         for(int i=1;i<freq.length;i++){
-            if(freq[i]==2){// freq count=2 means repeated number
+            if(freq[i]==2){
                 dup=i;
             }
-            if(freq[i]==0){//freq count=1 means missing number
+            if(freq[i]==0){
                 miss=i;
             }
         }
-        return new int[]{dup,miss};//returns both the elements
+        return new int[]{dup,miss};
     }
 }
