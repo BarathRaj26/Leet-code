47df0c5a3db2db08cbb84fb6346c071e8796af4a
BarathRaj26 <151170282+BarathRaj26@users.noreply.github.com>
2024-02-03 23:39:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (19.73%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length()<26){
+            return false;
+        }
+        else{
+            for(char i='a';i<='z';i++){
+                if(sentence.indexOf(i)<0){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
