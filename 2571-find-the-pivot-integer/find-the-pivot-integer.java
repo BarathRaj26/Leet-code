6521b68c6d9beb500e4f8211b0ca75d94d3e2aa1
BarathRaj26 <151170282+BarathRaj26@users.noreply.github.com>
2024-03-13 20:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (27.98%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int sum=n*(n+1)/2;
+        int root=(int)Math.sqrt(sum);
+        if(root*root==sum){
+            return root;
+        }
+        return -1;
+    }
+}
